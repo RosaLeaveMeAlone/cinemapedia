406e212c7699e045a493f1f5b51cb75be9628fff
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-31 10:43:19 -0400

    Seccion 12 Entidad - Repositories y Datasources

---
+++ lib/config/domain/datasources/movies_datasource.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:cinemapedia/config/domain/entities/movie.dart';
+
+abstract class MovieDatasource {
+
+  Future<List<Movie>> getNowPlaying( {int page = 1} );
+
+
+
+}
---
+++ lib/config/domain/repositories/movies_repository.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:cinemapedia/config/domain/entities/movie.dart';
+
+abstract class MovieRepository {
+
+  Future<List<Movie>> getNowPlaying( {int page = 1} );
+
+
+
+}
