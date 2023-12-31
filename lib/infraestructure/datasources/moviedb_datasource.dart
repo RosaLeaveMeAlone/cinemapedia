277543db0bd84503945a3101476fe1839dbce4c8
Joel Uzcategui <Joeluzcategui102@gmail.com>
2023-12-31 16:40:33 -0400

    Seccion 12 Realizar el mapeo de la respuesta de MovieDB

--- lib/infraestructure/datasources/moviedb_datasource.dart
+++ lib/infraestructure/datasources/moviedb_datasource.dart
@@ -4,6 +4,8 @@
 import 'package:cinemapedia/config/constants/enviroment.dart';
 import 'package:cinemapedia/config/domain/datasources/movies_datasource.dart';
 import 'package:cinemapedia/config/domain/entities/movie.dart';
+import 'package:cinemapedia/infraestructure/mappers/movie_mapper.dart';
+import 'package:cinemapedia/infraestructure/models/moviedb/moviedb_response.dart';
 import 'package:dio/dio.dart';
 
 class MoviedbDatasource extends MoviesDatasource {
@@ -23,8 +25,13 @@ class MoviedbDatasource extends MoviesDatasource {
   Future<List<Movie>> getNowPlaying({int page = 1}) async {
 
     final response = await dio.get('/movie/now_playing');
-    response.data;
-    final List<Movie> movies = [];
+    final movieDBResponde = MovieDbResponse.fromJson(response.data);
+    
+    final List<Movie> movies = movieDBResponde.results
+    .where((movieDb) => movieDb.posterPath != 'no-poster')
+    .map(
+      (moviedb) => MovieMapper.movieDBToEntity(moviedb)
+      ).toList();
 
 
 
