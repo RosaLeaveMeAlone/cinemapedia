10781d14af6d7506aba39f119a2e20d27172e19e
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-01-01 10:05:37 -0400

    Seccion 13 CustomBottomNavigationBar

--- lib/presentation/widgets/widgets.dart
+++ lib/presentation/widgets/widgets.dart
@@ -1,6 +1,8 @@
 
 
 
+export 'package:cinemapedia/presentation/widgets/shared/custom_bottom_navigation.dart';
+
 export 'package:cinemapedia/presentation/widgets/movies/movies_slideshow.dart';
 
 export 'package:cinemapedia/presentation/widgets/shared/custom_appbar.dart';
