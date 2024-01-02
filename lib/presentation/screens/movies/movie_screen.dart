f35500c56bb32547d811a4053189e6c26267bae7
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-01-01 22:12:58 -0400

    Seccion 14 Continuacion de la aplicacion fix

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+class MovieScreen extends StatelessWidget {
+
+  static const name = 'movie-screen';
+
+  final String modieId;
+
+  const MovieScreen({
+    super.key, 
+    required this.modieId
+    });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
