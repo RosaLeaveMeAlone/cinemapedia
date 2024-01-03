e0169121341c1488fa344d5eef29bf3bc35ff01a
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-01-02 21:29:43 -0400

    Seccion 15 Mostrar las peliculas en la busqueda

--- lib/infraestructure/mappers/movie_mapper.dart
+++ lib/infraestructure/mappers/movie_mapper.dart
@@ -16,8 +16,8 @@ class MovieMapper {
       popularity: moviedb.popularity,
       posterPath: (moviedb.posterPath != '')
       ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
-      : 'no-poster',
-      // : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
+      // : 'https://www.csaff.org/wp-content/uploads/csaff-no-poster.jpg',
+      : 'https://static.displate.com/857x1200/displate/2022-04-15/7422bfe15b3ea7b5933dffd896e9c7f9_46003a1b7353dc7b5a02949bd074432a.jpg',
       releaseDate: moviedb.releaseDate,
       title: moviedb.title,
       video: moviedb.video,
