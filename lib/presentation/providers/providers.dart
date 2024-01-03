5c6686199c8c283620a6572f01fc6f832b5b3fa1
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-01-03 19:37:55 -0400

    Seccion 17 Favorite MoviesProdiver

--- lib/presentation/providers/providers.dart
+++ lib/presentation/providers/providers.dart
@@ -9,3 +9,4 @@ export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
 export 'search/search_movie_provider.dart';
 export 'storage/local_storage_provider.dart';
+export 'storage/favorite_movies_provider.dart';
