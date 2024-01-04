e65044e4dd2e8b0e09a4ee71724a60e44f399ee7
Joel Uzcategui <Joeluzcategui102@gmail.com>
2024-01-03 20:25:50 -0400

    Seccion 17 Mostrar Peliculas Favoritas & Resolucion de la tarea

--- lib/presentation/views/movies/favorites_view.dart
+++ lib/presentation/views/movies/favorites_view.dart
@@ -1,18 +1,55 @@
+import 'package:cinemapedia/config/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     body: ListView.builder(
+  //       itemBuilder: (context, index) {
+  //         return ListTile(
+  //           title: Text('Movie $index'),
+  //         );
+  //       }
+  //       ),
+  //   );
+  // }
+  
+  @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites view'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
-      ),
+
+
+    final Map<int, Movie> favorites = ref.watch(favoriteMoviesProvider);
+
+    final List<MapEntry<int, Movie>> favoriteList = favorites.entries.toList();
+
+    return ListView.builder(
+      itemCount: favoriteList.length,
+      itemBuilder: (context, index) {
+        final movie = favoriteList[index].value;
+        return ListTile(
+          title: Text(movie.title),
+        );
+      }
     );
   }
+
+  
 }
